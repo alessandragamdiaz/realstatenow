17878147f60e5c55f40674ff943894665e46c4fe
alessandra gamboa <116472494+alessandragamdiaz@users.noreply.github.com>
2024-05-17 22:02:08 +0100

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -11,7 +11,7 @@ import jwtCheck from "../config/auth0Config.js";
 const router = express.Router();
 
 router.post("/register", createUser);
-router.post("/bookVisit/:id",bookVisit);
+router.post("/bookVisit/:id", bookVisit);
 router.post("/allBookings", getAllBookings);
 router.post("/removeBooking/:id", cancelBooking);
 router.post("/toFav/:rid", toFav);
